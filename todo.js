dee5b76553718873c28817ac1b4417873eafd259
조현일 <himzei@gmail.com>
2023-12-13 23:22:23 +0900

    delete complete

--- todo.js
+++ todo.js
@@ -10,27 +10,37 @@ function saveToDos() {
 
 function deleteToDo(event) {
   const li = event.target.parentElement;
+  // console.log(li.id)
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
 
 function paintToDo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
   const button = document.createElement("button");
   button.innerText = "❌";
   button.addEventListener("click", deleteToDo);
   li.appendChild(span);
   li.appendChild(button);
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   toDoList.appendChild(li);
 }
 
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
+  console.log(newTodo);
   toDoInput.value = "";
-  toDos.push(newTodo);
-  paintToDo(newTodo);
+  // 삭제를 위한
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
   saveToDos();
 }
 
@@ -38,7 +48,7 @@ toDoForm.addEventListener("submit", handleToDoSubmit);
 
 const savedToDos = localStorage.getItem("todos");
 
-if (saveToDos !== null) {
+if (savedToDos !== null) {
   const parsedToDos = JSON.parse(savedToDos);
   toDos = parsedToDos;
   parsedToDos.forEach(paintToDo);
